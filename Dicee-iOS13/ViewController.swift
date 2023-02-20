c7c2c51283b5276abeb196c33236eb4c6144fbbe
ArilsonFJS <arilsonsilva61@gmail.com>
2023-02-20 15:21:13 -0300

    IBOutlets

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
     }
 
 
